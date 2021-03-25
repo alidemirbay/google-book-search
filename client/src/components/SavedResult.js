e9fd4f1a5c269e2f870e8ced5e30892a06a814c5
alidemirbay <alidemirbay@gmail.com>
2021-03-25 12:02:41 -0700

    add route

--- client/src/components/SavedResult.js
+++ client/src/components/SavedResult.js
@@ -12,12 +12,10 @@ const SavedResult = (props) => {
 
                 <div className="row">
 
-
                   <div className='col-8'>
                     <h4 className="">{savedbook.title}</h4>
                     <h5 className="">{savedbook.authors}</h5>
                   </div>
-
                   <div className='col-4  justify-content-end'>
                     <a href={savedbook.link} target="_blank" rel="noreferrer"> {" "} <button className=" btn btn-success m-2"> VIEW</button>{" "} </a>
                     <button className=" btn btn-danger m-2" id={savedbook._id} onClick={() => props.handleDeleteButton(savedbook._id)} > DELETE{" "} </button>
@@ -25,20 +23,12 @@ const SavedResult = (props) => {
 
                 </div>
 
-
-
-
-
                 <div className="row" id={savedbook.title + "Card"} key={savedbook._id} >
                   <div className="col-2"> {" "} <img src={savedbook.image} alt={savedbook.title} />{" "} </div>
                   <div className="col-10 pl-2">
-
                     <p className="pr-3">{savedbook.description}</p>
                   </div>
                 </div>
-
-
-
 
 
               </div>
